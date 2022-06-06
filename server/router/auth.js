ad1e53a0aec45436895892fb4b38f97908eb6230
Muhammad Ammar Tanweer <49334070+MUHAMMAD-AMMAR-TANWEER@users.noreply.github.com>
2022-06-06 23:14:50 +0500

    all changes are done

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,211 +1,231 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-require('../db/conn');
-const User  = require('../models/userSchema');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const Post = require("../models/Feed"); 
+require("../db/conn");
+const User = require("../models/userSchema");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Post = require("../models/Feed");
 const TimPost = require("../models/TimFeed");
 
-router.get('/', (req,res) =>{
-    res.send('Hello from the server routers');
+router.get("/", (req, res) => {
+  res.send("Hello from the server routers");
 });
 
-router.post('/registers',async (req,res) => {
-    const {name , email, device, password, cpassword} = req.body;
-    if (!name  || !email || !device || !password || !cpassword) {
-        return res.status(422).json({message: "Please fill the form properly"});
+router.post("/registers", async (req, res) => {
+  const { name, email, device, password, cpassword } = req.body;
+  if (!name || !email || !device || !password || !cpassword) {
+    return res.status(422).json({ message: "Please fill the form properly" });
+  }
+  try {
+    const userExist = await User.findOne({ email: email });
+    const deviceExist = await User.findOne({ device: device });
+    if (userExist) {
+      return res.status(422).json({ error: "Email Already registered" });
+    } else if (deviceExist) {
+      return res.status(422).json({ error: "Device is Already registered" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "Password is not matching" });
+    } else {
+      const user = new User({ name, email, device, password, cpassword });
+      await user.save();
+      res.status(201).json({ message: "User successfully registered" });
     }
-    try{
-        const userExist = await User.findOne({email:email});
-        const deviceExist = await User.findOne({device:device});
-        if (userExist) {
-            return res.status(422).json({error: "Email Already registered"});
-        }
-        else if (deviceExist){
-            return res.status(422).json({error: "Device is Already registered"});
-
-        }
-        else if (password != cpassword){
-            return res.status(422).json({error:"Password is not matching"});
-        }
-        else {
-            const user = new User({name , email, device, password, cpassword});
-            await user.save();
-            res.status(201).json({message:"User successfully registered"});
-        }
-        //yaha hash karna hai 
-    }
-
-    catch (err) {
-        console.log(err);
-    }
+    //yaha hash karna hai
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/login', async (req,res) =>{
-    try{
-        
-        const {email, password} = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).json({error:"Please fill the data"});
-        }
-
-        const userLogin = await User.findOne({email:email});
-
-
-        if (userLogin){
-            const isMatch = await bcrypt.compare(password, userLogin.password);
-
-            const token = await userLogin.generateAuthToken();
-            res.cookie("jwtoken",token,{
-                expires: new Date(Date.now() +25892000000),
-                httpOnly:false,
-                secure:true
-
-            });
-            console.log(token);
-
-
-
-            if (!isMatch){
-                res.status(400).json({error:"Invalid credinatials  "});
-            }
-            else {
-                res.status(200).json({message: "User Login Successfully","Device":userLogin.device});
-                
-            }
-        }
-        else {
-            res.status(400).json({error:"Invalid credinatials"});       
-        }
-
+    if (!email || !password) {
+      return res.status(400).json({ error: "Please fill the data" });
     }
-    catch(err){
-        console.log(err);
 
-    }
-});
+    const userLogin = await User.findOne({ email: email });
 
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password);
 
-router.post("/feed" , async (req,res)=>{
+      const token = await userLogin.generateAuthToken();
+      // res.cookie("jwtoken", token, {
+      //   expires: new Date(Date.now() + 25892000000),
+      //   httpOnly: false,
+      //   secure: true,
+      // });
+      // console.log(token);
 
-
-    if (!req.body.Device)   {
-        res.status(400).send({ message: "Please enter Device UID" });
-        return;
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid credinatials  " });
+      } else {
+        res.status(200).json({
+          message: "User Login Successfully",
+          Device: userLogin.device,
+          token: token,
+        });
       }
+    } else {
+      res.status(400).json({ error: "Invalid credinatials" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-    const postt = new Post({
-        Device:req.body.Device,
-        Car:req.body.Car,
-        Bicycle:req.body.Bicycle,
-        Person:req.body.Person,
-        Bus:req.body.Bus,
-        Motorcycle:req.body.Motorcycle,
-        Truck:req.body.Truck,
+router.post("/feed", async (req, res) => {
+  if (!req.body.Device) {
+    res.status(400).send({ message: "Please enter Device UID" });
+    return;
+  }
 
-    
-    })
+  const postt = new Post({
+    Device: req.body.Device,
+    Car: req.body.Car,
+    Bicycle: req.body.Bicycle,
+    Person: req.body.Person,
+    Bus: req.body.Bus,
+    Motorcycle: req.body.Motorcycle,
+    Truck: req.body.Truck,
+  });
 
-    await postt
+  await postt
     .save()
     .then((postt) => {
       res.status(200).send({ message: "Done" });
     })
-    .catch((err) =>
-      res.status(400).send({ message: err })
-      
-    );
-
-
+    .catch((err) => res.status(400).send({ message: err }));
 });
 
-router.get("/gfeed", async (req,res) => {
-    
-    
-    var sendat = [];
-    var carlst = []; var motolst = []; var trucklist = [] ; var buslist = []; var perlst = []; var cyclst = [];
-    for (let i =0 ;i<31;i++){
-        let now = new Date();
-        var tod = new Date(now.setDate(now.getDate() - i));
-        var prv = new Date(now.setDate(now.getDate() - i-1));
+router.get("/gfeed", async (req, res) => {
+  var sendat = [];
+  var carlst = [];
+  var motolst = [];
+  var trucklist = [];
+  var buslist = [];
+  var perlst = [];
+  var cyclst = [];
+  for (let i = 0; i < 31; i++) {
+    let now = new Date();
+    var tod = new Date(now.setDate(now.getDate() - i));
+    var prv = new Date(now.setDate(now.getDate() - i - 1));
 
-        let event_query = {"$gte":prv,"$lte":tod};
-        const sum = await Post.aggregate([{$match : {"createdAt":event_query}},{$group:{_id:{Device:req.params.Device},
-        "Car":{$sum:"$Car"}, "Person":{$sum:"$Person"},"Bicycle":{$sum:"$Bicycle"},"Bus":{$sum:"$Bus"},
-        "Motorcycle":{$sum:"$Motorcycle"},"Truck":{$sum:"$Truck"}}}
-        ,{$project:{_id:0,"Cars":"$Car","Person":"$Person","Bicycles":"$Bicycle","Buses":"$Bus","Motorcycles":"$Motorcycle","Trucks":"$Truck"}}]) //suming whole
-        if (sum.length>0) {
-            carlst.push(sum[0]["Cars"]); buslist.push(sum[0]["Buses"]); perlst.push(sum[0]["Person"]);cyclst.push(sum[0]["Bicycles"]);motolst.push(sum[0]["Motorcycles"]);trucklist.push(sum[0]["Trucks"]);
-            sendat.push(new Date(now.setDate(now.getDate() - i)).toLocaleDateString('en-US'));
-
-        }
-        else {
-            continue
-        }
-
+    let event_query = { $gte: prv, $lte: tod };
+    const sum = await Post.aggregate([
+      { $match: { createdAt: event_query } },
+      {
+        $group: {
+          _id: { Device: req.params.Device },
+          Car: { $sum: "$Car" },
+          Person: { $sum: "$Person" },
+          Bicycle: { $sum: "$Bicycle" },
+          Bus: { $sum: "$Bus" },
+          Motorcycle: { $sum: "$Motorcycle" },
+          Truck: { $sum: "$Truck" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          Cars: "$Car",
+          Person: "$Person",
+          Bicycles: "$Bicycle",
+          Buses: "$Bus",
+          Motorcycles: "$Motorcycle",
+          Trucks: "$Truck",
+        },
+      },
+    ]); //suming whole
+    if (sum.length > 0) {
+      carlst.push(sum[0]["Cars"]);
+      buslist.push(sum[0]["Buses"]);
+      perlst.push(sum[0]["Person"]);
+      cyclst.push(sum[0]["Bicycles"]);
+      motolst.push(sum[0]["Motorcycles"]);
+      trucklist.push(sum[0]["Trucks"]);
+      sendat.push(
+        new Date(now.setDate(now.getDate() - i)).toLocaleDateString("en-US")
+      );
+    } else {
+      continue;
     }
-    var finalData = {"Cars":carlst,"Bicycles":cyclst,"Buses":buslist,"Trucks":trucklist,"Person":perlst,"Motorcycles":motolst,"Labels":sendat};
+  }
+  var finalData = {
+    Cars: carlst,
+    Bicycles: cyclst,
+    Buses: buslist,
+    Trucks: trucklist,
+    Person: perlst,
+    Motorcycles: motolst,
+    Labels: sendat,
+  };
 
-    res.status(200).send(finalData);
+  res.status(200).send(finalData);
 });
 
 //Post api for the server (csv data)
-router.post('/timfeed' , async (req,res) => {
-    console.log(req.body.Device);
-    console.log(req.body.DatTim);
-    if (!req.body.Device)   {
-        res.status(400).send({ message: "Please enter Device UID" });
-        return;
-      }
-      
-    const timpost = new TimPost({
-        Device:req.body.Device,
-        DetectionID:req.body.DetectionID,
-        DatTim:req.body.DatTim,
-        Detection:req.body.Detection,
-    
-    })
-    console.log(timpost);
+router.post("/timfeed", async (req, res) => {
+  console.log(req.body.Device);
+  console.log(req.body.DatTim);
+  if (!req.body.Device) {
+    res.status(400).send({ message: "Please enter Device UID" });
+    return;
+  }
 
-    await timpost
+  const timpost = new TimPost({
+    Device: req.body.Device,
+    DetectionID: req.body.DetectionID,
+    DatTim: req.body.DatTim,
+    Detection: req.body.Detection,
+  });
+  console.log(timpost);
+
+  await timpost
     .save()
     .then((timpost) => {
       res.status(200).send({ message: "Done" });
     })
-    .catch((err) =>
-      res.status(400).send({ message: err })
-      
-    );
-    
-
+    .catch((err) => res.status(400).send({ message: err }));
 });
 
-router.get('/gtimfeed/:Device/:StartDat/:EndDat' , async (req, res) => {
-    console.log(req.params);
-    var Data =[];
-    if (!req.params.Device)   {
-        res.status(400).send({ message: "Please enter Device UID" });
-        return;
-      }
-    if (!req.params.StartDat && !req.params.EndDat){
-        res.status(400).send({message:"Please Enter both Dates"});
-        return;
-    }
-    var prvDat = new Date(req.params.StartDat);
-    var NxtDat = new Date(req.params.EndDat);
-    const event_query = {"$gte": prvDat, "$lte":NxtDat};
-    const docs = await TimPost.aggregate([{"$match":{"createdAt":event_query}}, {"$group" :{_id:{Device:req.params.Device}, "DetectionID":{"$push": "$DetectionID"},"DatTim":{"$push": "$DatTim"},"Detection":{"$push": "$Detection"}}},
+router.get("/gtimfeed/:Device/:StartDat/:EndDat", async (req, res) => {
+  console.log(req.params);
+  var Data = [];
+  if (!req.params.Device) {
+    res.status(400).send({ message: "Please enter Device UID" });
+    return;
+  }
+  if (!req.params.StartDat && !req.params.EndDat) {
+    res.status(400).send({ message: "Please Enter both Dates" });
+    return;
+  }
+  var prvDat = new Date(req.params.StartDat);
+  var NxtDat = new Date(req.params.EndDat);
+  const event_query = { $gte: prvDat, $lte: NxtDat };
+  const docs = await TimPost.aggregate([
+    { $match: { createdAt: event_query } },
+    {
+      $group: {
+        _id: { Device: req.params.Device },
+        DetectionID: { $push: "$DetectionID" },
+        DatTim: { $push: "$DatTim" },
+        Detection: { $push: "$Detection" },
+      },
+    },
+  ]);
+  console.log(docs);
 
-])
-for (let i = 0 ; i<docs[0].DetectionID.length; i++){
-    for (let j = 0; j<docs[0].Detection[i].length;j++){
-        Data.push({"DateTime":docs[0].DatTim[i][j],"Detection":docs[0].Detection[i][j],"DetectionID":docs[0].DetectionID[i][j]})
+  for (let i = 0; i < docs[0].DetectionID.length; i++) {
+    for (let j = 0; j < docs[0].Detection[i].length; j++) {
+      Data.push({
+        DateTime: docs[0].DatTim[i][j],
+        Detection: docs[0].Detection[i][j],
+        DetectionID: docs[0].DetectionID[i][j],
+      });
     }
-}
+  }
 
-res.status(200).send({message:Data});
-})
+  res.status(200).send({ message: Data });
+});
 module.exports = router;
-
